be975b00ecb7a4863df093542afac4cf16844cc9
Xiuyuan <xiuyuanyu86@gmail.com>
2024-12-12 11:07:12 +0800

    remain k way merge to implement

--- main.py
+++ main.py
@@ -10,11 +10,65 @@ def external_merge_sort(B, b, N, T, input_file, output_file):
 
     sec_store.read_file(input_file)
 
+    nums = buffer_pool_manager.allocate(B // b // 2)
+    buf = buffer_pool_manager.allocate(b // b // 2)
+    for k in range(0, N, B // 2):
+        sec_man.read("input", k, B // 2, nums)
+        merge_sort(nums, buf, k, k + len(nums) - 1)
+        sec_man.write(f"sort{k}", k, B // 2, nums)
+
+    num_run = N // B * 2
+    num_block = B // b
+
+    block_per_run = num_block // (num_run + 1)
+    block_buf = num_block - num_run * block_per_run
+    runs = []
+    for k in range(0, num_run):
+        runs.append(buffer_pool_manager.allocate(block_per_run))
+        sec_man.read(f"sort{k}", 0, block_per_run * b, runs[k])
+    
+    buf = buffer_pool_manager.allocate(block_buf)
+
+
+def merge(nums, buf, left: int, mid: int, right: int):
+    """merge the two sub array"""
+    i, j, k = left, mid + 1, left
+
+    while i <= mid and j <= right:
+        if nums[i] <= nums[j]:
+            buf[k] = nums[i]
+            i += 1
+        else:
+            buf[k] = nums[j]
+            j += 1
+        k += 1
+
+    while i <= mid:
+        buf[k] = nums[i]
+        i += 1
+        k += 1
+    while j <= right:
+        buf[k] = nums[j]
+        j += 1
+        k += 1
+
+    for k in range(left, right + 1):
+        nums[k] = buf[k]
+
+def merge_sort(nums, buf, left: int, right: int):
+    """Merge sort the array"""
+
+    if left >= right:
+        return
+    mid = (left + right) // 2
+    merge_sort(nums, buf, left, mid)
+    merge_sort(nums, buf, mid + 1, right)
+    merge(nums, buf, left, mid, right)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("INPUT", help="Path to input file", dest="input")
-    parser.add_argument("OUTPUT", help="Path to output file", dest="output")
+    parser.add_argument("input", help="Path to input file")
+    parser.add_argument("output", help="Path to output file")
     args = parser.parse_args()
 
     # Parameters
